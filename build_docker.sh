3d66e8c84693aa902545efdac68e74faab612a76
Neil Blake <neil0blake@gmail.com>
2024-10-09 19:28:07 +1300

    fix build

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 245511142268.dkr.ecr.us-west-2.amazonaws.com
+
+# Get the AWS account ID
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com
 docker buildx build \
      --push \
      --platform linux/amd64 \
-     -t 245511142268.dkr.ecr.us-west-2.amazonaws.com/cell-segmentation-job:latest \
+     -t ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/cell-segmentation-job:latest \
      -f docker/Dockerfile \
      .
